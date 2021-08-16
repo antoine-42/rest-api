d3c6c14f499375ff9d35c8e879c6e1e31c55ec8a
Antoine Dujardin <antoinedujardin42@gmail.com>
2021-08-16 13:54:03 +0200

    add models

---
+++ game_api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Studio(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    release_date = models.DateField()
+    studio = models.ForeignKey(Studio, related_name="games", on_delete=models.SET_NULL)
+    ratings = models.IntegerField()
+    platforms = models.ManyToManyField(Platform, related_name="games")
